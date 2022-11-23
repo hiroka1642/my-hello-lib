6acfd5661d729d2408df657a68605de660c1edd9
hiroka1642 <uver7770422@gmail.com>
2022-11-23 16:59:06 +0900

    ✅ 単体テスト

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+// 型定義のインポート
+import { Config } from "@jest/types";
+
+// オプションを設定
+const config: Config.InitialOptions = {
+  // Jest の設定のベース
+  preset: "ts-jest",
+  // test ファイルの置き場所
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  // Node.js 環境向けにテストする
+  testEnvironment: "node",
+  // カバレッジ（テストコードの捕捉率）を収集する
+  collectCoverage: true,
+};
+
+// 設定を default エクスポートします
+export default config;
